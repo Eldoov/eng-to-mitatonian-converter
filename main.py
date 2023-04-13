5d22ef3e2c7234e5dc2efc9ec062f1b445c6b2fd
Zuowen Tang <zuowent@bu.edu>
2023-04-12 21:39:28 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+import eng_to_ipa as ipa
+from PIL import Image, ImageFont, ImageDraw
+import translator
+
+
+def outputPic(text, fontSize=30):
+    font = ImageFont.truetype("Mitatonian-Regular", fontSize, encoding='utf-8')
+    img = Image.new("RGB", (1000, 50), (255, 255, 255))
+    draw = ImageDraw.Draw(img)
+    draw.text((0, 0), text, (0, 0, 0), font=font)
+    img.show()
+
+
+def translate(text):
+    text = ipa.convert(text)
+    new_text = translator.get(text)
+    return new_text
+
+
+def transFile(filePath):
+    final_line = ''
+    for line in open(filePath):
+        new_line = translate(line)
+        final_line = new_line + final_line
+    outputPic(final_line, 30)
+
+
+def Start():
+    while True:
+        choice = None
+        while choice is None:
+            try:
+                print("Welcome to English-to-Mitatonian converter.")
+                print("Do you want to:")
+                print("1. Translate a sentence. | 2. Translate a file. | 0. Quit.")
+                choice = int(input())
+            except ValueError:
+                print("Invalid input!")
+                continue
+            if choice < 0 or choice > 2:
+                print("Invalid input.")
+                choice = None
+                continue
+
+        if choice == 1:
+            print('Enter your sentence:')
+            text = input()
+            res = translate(text)
+            outputPic(res)
+            continue
+        elif choice == 2:
+            transFile('text.txt')
+            continue
+        elif choice == 0:
+            quit()
+
+
+Start()
+
---
+++ translator.py
@@ -0,0 +1,50 @@
+dictionary = {'i': 'i', 'ɪ': '1', 'e': 'e', 'ɛ': '3', 'æ': '5', 'ʌ': '6', 'ə': '2',
+              'ɚ': '', 'u': 'u', 'ʊ': 'o', 'o': '7', 'ɔ': '4', 'ɑ': 'a',
+              'p': '-', 'b': 'b', 'd': 'd', 't': 't', 'k': 'k', 'g': 'g', 'f': 'f', 'v': 'v',
+              'ɵ': '\'', 'ð': ';', 's': 's', 'z': 'z', 'ʃ': 'p', 'ʒ': 'q', 'h': 'h', 'ʧ': 'y',
+              'ʤ': 'x', 'm': 'm', 'n': 'n', 'ŋ': 'c', 'l': 'l', 'r': 'r', 'w': 'w', 'j': 'j',
+              '0': '9', '1': '[', '2': '8', '3': '\\', '4': ']', '5': '0', '6': '/', '7': '=',
+              'ˈ': ''}
+
+
+def changeDiph(text):
+    diphthongList = ['eɪ', 'oʊ', 'ɪə', 'ʊə', 'əʊ', 'ɑɪ', 'ɑʊ', 'ɔɪ']
+    for i in range(8):
+        text = text.replace(diphthongList[i], str(i))
+    return text
+
+
+def addSpace(text):
+    text = " ".join(text)
+    text = text + " "
+    return text
+
+
+def transLetter(letter, dic):
+    if letter in dic:
+        return dic[letter]
+    else:
+        return letter
+
+
+def translate(text):
+    text = addSpace(text)  # add space for dictionary to recognize
+    translation = ''
+    word = ''
+    for c in text:
+        if c != ' ':
+            word = word + c
+        else:
+            translation = translation + ' ' + transLetter(word, dictionary)
+            word = ''
+
+    translation = translation.replace('  ', '*')
+    translation = translation.replace(' ', '')
+    translation = translation.replace('**', ',')
+    return translation.replace("*", ",")  # remove extra space
+
+
+def get(text):
+    translation = translate(text)
+    return translation
+
